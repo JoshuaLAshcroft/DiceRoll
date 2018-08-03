da7a916ee2d3246bcc41dd7c1d63ac91b49dd4cf
JoshuaLAshcroft <34969512+JoshuaLAshcroft@users.noreply.github.com>
2018-08-03 23:18:12 +0100

    Python Code

---
+++ diceRoll.py
@@ -0,0 +1,15 @@
+
+#
+# DICE ROLLING SIMULATOR
+# version 1
+#
+
+import random
+
+while True:
+    start = str(input("Roll? [Input Y/N]\n> "))
+    if start.lower() == "y":
+        number = random.randint(0,6);
+        print(number)
+    else:
+        exit()
